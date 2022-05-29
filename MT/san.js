359a22a9b282e22354b1f2f4a47b415fee017f79
tanghaotian <89062432+tanghaoxu@users.noreply.github.com>
2022-05-30 00:08:32 +0800

    Update san.js

--- MT/san.js
+++ MT/san.js
@@ -35,7 +35,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
   ;(async () => {
     let panel_result = {
-      title: '🖥流媒体解锁检测 🚦',
+      title: '🖥流媒体解锁检测🚥',
       content: '',
       icon: 'play.tv.fill',
       'icon-color': '#FF0097',
